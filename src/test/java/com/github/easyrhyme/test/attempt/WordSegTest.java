d12cc3dacb9d9f33ad296ade8d0fe42232c6a99c
ziqiang.deng <ziqiang.deng@qunar.com>
2017-12-14 23:41:36 +0800

    routine commit

---
+++ src/test/java/com/github/easyrhyme/test/attempt/WordSegTest.java
@@ -0,0 +1,23 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2012 Qunar.com. All Rights Reserved.
+ */
+package com.github.easyrhyme.test.attempt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+//--------------------- Change Logs----------------------
+// <p>@author dengziqiang Initial Created at 2017年12月14日<p>
+//-------------------------------------------------------
+public class WordSegTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
